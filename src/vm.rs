b11caf63276e672eb1a69bb6939fbe60f156c377
thisLucio <luxusnietzsche@gmail.com>
2023-04-14 01:38:37 -0300

    Add tests for opcode HLT and IGL

--- src/vm.rs
+++ src/vm.rs
@@ -1,13 +1,42 @@
+use crate::instruction::Opcode;
+
 pub struct VM {
-    registers: [i32; 32]
+    registers: [i32; 32],
+    pc: usize,
+    program: Vec<u8>
 }
 
 
 impl VM {
     pub fn new() -> VM {
         VM{
-            registers: [0; 32]
+            registers: [0; 32],
+            program: vec![],
+            pc: 0,
         }
+    }
+
+    pub fn run(&mut self){
+        loop{
+            if self.pc >= self.program.len(){
+                break;
+            }
+            match self.decode_opcode() {
+                Opcode::HLT => {
+                    println!("HLT encountered");
+                    return;
+                },
+                _ => {
+                    println!("Unrecognized opcode  found! Terminating!");
+                    return;
+                }
+            }
+        }
+    }
+    fn decode_opcode(&mut self) -> Opcode {
+        let opcode = Opcode::from(self.program[self.pc]);
+        self.pc += 1;
+        return opcode;
     }
 }
 
@@ -20,5 +49,24 @@ mod tests {
     fn test_create_vm(){
         let test_vm = VM::new();
         assert_eq!(test_vm.registers[0], 0)
+    }
+
+
+    #[test]
+    fn test_opcode_hlt() {
+        let mut test_vm = VM::new();
+        let test_bytes = vec![0,0,0,0];
+        test_vm.program = test_bytes;
+        test_vm.run();
+        assert_eq!(test_vm.pc, 1);
+    }
+
+    #[test]
+    fn test_opcode_igl() {
+        let mut test_vm = VM::new();
+        let test_bytes = vec![200,0,0,0];
+        test_vm.program = test_bytes;
+        test_vm.run();
+        assert_eq!(test_vm.pc, 1);
     }
 }

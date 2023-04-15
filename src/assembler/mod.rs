15407959c63478c4994d534f753756a4bcfc41e4
thisLucio <luxusnietzsche@gmail.com>
2023-04-15 00:54:43 -0300

    Feat: New Assembler

---
+++ src/assembler/mod.rs
@@ -0,0 +1,16 @@
+pub mod opcode_parsers;
+pub mod register_parsers;
+pub mod operand_parsers;
+pub mod instruction_parsers;
+pub mod program_parsers;
+
+use instruction::Opcode;
+use crate::instruction;
+
+
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Op{code: Opcode},
+    Register{reg_num: u8},
+    IntegerOperand{value: i32},
+}
